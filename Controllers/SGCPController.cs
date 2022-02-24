6cd0a860fde72c5c8135f26e2f6dbe8ff56cbca0
Alexandre Morato <49573908+alexmorato1969@users.noreply.github.com>
2022-02-24 00:13:36 -0300

    Add project files.

---
+++ Controllers/SGCPController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SGCP_EGS.Controllers
+{
+    public class SGCPController : Controller
+    {
+        // GET: SGCP
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SGCP_EGS.MvcApplication" Language="C#" %>
